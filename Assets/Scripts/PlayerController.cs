fce3162617e0a1f66f5229c01ec5fa492f5f1c17
1f1n1ty <matthew.zhang.306@gmail.com>
2019-10-04 11:06:41 -0500

    very basic movement and jumping code
    next up: implementing the groundcheck

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public float maxXSpeed;
+    public float xAccel;
+    public float jumpPower;
+
+    Rigidbody2D rb2d;
+    SpriteRenderer sr;
+
+
+    float xInput;
+    bool jInput;
+    bool oldJInput;
+
+
+    private void Start() {
+        rb2d = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>(); // will be deleted when the sprite is no longer attached to the same gameobject as the script
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        // get inputs
+        xInput = Input.GetAxisRaw("Horizontal");
+        jInput = Input.GetAxisRaw("Jump") > 0;
+    }
+
+    private void FixedUpdate() {
+        Vector2 vel = rb2d.velocity;
+
+        if (xInput != 0) {
+            // accelerate player in the direction of motion
+            vel.x += xInput * xAccel;
+            vel.x = Mathf.Clamp(vel.x, -maxXSpeed, maxXSpeed);
+        }
+        else {
+            if (Mathf.Abs(vel.x) < xAccel) {
+                // player should stop
+                vel.x = 0;
+            }
+            else {
+                // player's speed should decrease
+                vel.x -= Mathf.Sign(vel.x) * xAccel;
+            }
+        }
+
+        if (jInput && !oldJInput) {
+            vel.y = jumpPower;
+        }
+
+        rb2d.velocity = vel;
+
+        // indicate that we have used the current jump input
+        oldJInput = jInput;
     }
 }
