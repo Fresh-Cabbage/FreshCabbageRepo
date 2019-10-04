db04a44e726176e88f571af0d33542d2dfb24eec
1f1n1ty <matthew.zhang.306@gmail.com>
2019-10-04 11:20:22 -0500

    very basic collision checking script
    this script will cause bugs later! but for now it should be fine

---
+++ Assets/Scripts/CollisionCheck.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CollisionCheck : MonoBehaviour
+{
+    [SerializeField] private bool isColliding;
+    public bool IsColliding { get { return isColliding; }}
+
+    public List<string> targetTags;
+
+    
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (targetTags.Any(tag => other.CompareTag(tag))) {
+            isColliding = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other) => OnTriggerEnter2D(other);
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (targetTags.Any(tag => other.CompareTag(tag))) {
+            isColliding = false;
+        }
+    }
+}
