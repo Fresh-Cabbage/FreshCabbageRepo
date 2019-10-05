c1e34957e729bfb515e95a2c5329289be59ce37a
1f1n1ty <matthew.zhang.306@gmail.com>
2019-10-05 11:27:48 -0500

    very basic camera follow
    added CameraController.cs

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player; // camera will be connected to the player another way when scene flow starts being implemented
+    
+    public float lerpSpeed;
+
+    Vector3 targetPosition;
+
+    private void FixedUpdate() {
+        if (player != null)
+            targetPosition = player.transform.position.WithZ(transform.position.z);
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, lerpSpeed);
+    }
+}
