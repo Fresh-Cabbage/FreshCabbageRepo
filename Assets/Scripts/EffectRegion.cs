f9fcd26c17c44b82d2df0aa8127e20506590be50
1f1n1ty <matthew.zhang.306@gmail.com>
2019-10-05 16:31:48 -0500

    effect region prefab
    includes new material, tag, prefab, script, and scene

---
+++ Assets/Scripts/EffectRegion.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectRegion : MonoBehaviour
+{
+    public LineRenderer lr;
+    public float circleRadius;
+
+    private void Start() {
+        DrawCircle(circleRadius);
+    }
+
+
+
+    void DrawCircle(float radius) {
+        Vector3[] points = new Vector3[36];
+        
+        for (int p = 0; p < points.Length; p++) {
+            points[p] = Quaternion.Euler(0, 0, 360f * p / points.Length) * Vector3.right * radius;
+        }
+
+        lr.positionCount = 36;
+        lr.SetPositions(points);
+        lr.gameObject.SetActive(true);
+    }
+}
