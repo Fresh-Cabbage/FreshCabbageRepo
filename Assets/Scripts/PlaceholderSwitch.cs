95c77ff89d47e39e33cbbd787797bb392a73f32d
1f1n1ty <matthew.zhang.306@gmail.com>
2019-10-09 17:03:05 -0500

    add placeholder switch
    this switch demonstrates the ability to detect whether a gameobject is in the effect region by doing the dummy action of turning a door on and off

---
+++ Assets/Scripts/PlaceholderSwitch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceholderSwitch : MonoBehaviour
+{
+    SpriteRenderer sr;
+    CollisionCheck effectCheck;
+    public GameObject door;
+
+    public Color offColor;
+    public Color onColor;
+
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        effectCheck = GetComponent<CollisionCheck>();
+    }
+
+    private void Update() {
+        sr.color = effectCheck.IsColliding ? onColor : offColor;
+        door.SetActive(!effectCheck.IsColliding);
+    }
+}
