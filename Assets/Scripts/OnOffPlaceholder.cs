bc8e16cea8f364cc11510fe8aa2f2741253129d7
gluethulhu <thexanbert@gmail.com>
2019-10-14 12:10:48 -0500

    OnOff stuff
    Added OnOff Block prefab and script
    
    added a miniscule amount of extra material to Level test

---
+++ Assets/Scripts/OnOffPlaceholder.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnOffPlaceholder : MonoBehaviour
+{
+    SpriteRenderer sr;
+    CollisionCheck effectCheck;
+    public Collider2D surface;
+
+    public bool defaultState;
+
+    public Color offColor;
+    public Color onColor;
+
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        effectCheck = GetComponent<CollisionCheck>();
+        surface = GetComponent<Collider2D>();
+    }
+
+    private void Update()
+    {
+        sr.color = effectCheck.IsColliding ? onColor : offColor;
+        if(effectCheck.IsColliding ? !defaultState : defaultState)
+        {
+            surface.enabled = true;
+        }
+    }
+}
