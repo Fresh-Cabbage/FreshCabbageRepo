38abf1e07e1f4fbce5ab8a87b9fb22ecdeff71b4
Wenhan Samuel Lee <wenhan_lee@utexas.edu>
2019-10-18 19:02:22 -0500

    Added "FinishTotem" and "FinishScreen". If the player touches "FinishTotem", "FinishScreen" becomes active.

---
+++ Assets/FinishTotem.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishTotem : MonoBehaviour
+{
+    public GameObject[] finishScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        finishScreen = GameObject.FindGameObjectsWithTag("FinishScreen");
+        foreach (GameObject obj in finishScreen)
+        {
+            obj.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            foreach (GameObject obj in finishScreen)
+            {
+                obj.SetActive(true);
+            }
+        }
+    }
+
+}
