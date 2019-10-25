29da85b33e5a6247264bbb856d69f3238b37d897
gluethulhu <thexanbert@gmail.com>
2019-10-25 12:36:27 -0500

    Trampolines (nonfunctional)
    Added TrampTest scene, Trampoline prefab and TrampolineCheck script
    
    Trampolines do not work at all yet and just makes the game think you're falling. Fix coming soonish

---
+++ Assets/Scripts/TrampolineCheck.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrampolineCheck : MonoBehaviour
+{
+    public float bounceStrength = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Rigidbody2D rb;
+        if (collision.CompareTag("Player") || collision.CompareTag("Totem"))
+        {
+            GameObject other = collision.gameObject;
+
+            rb = other.GetComponent<Rigidbody2D>();
+
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y + bounceStrength);
+        }
+    }
+} //Fun fact: Unity makes me angy.
