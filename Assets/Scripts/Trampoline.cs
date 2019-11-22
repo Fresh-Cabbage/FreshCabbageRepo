dc42a0291a9d77f2c28129313d73fe6381924421
1f1n1ty <matthew.zhang.306@gmail.com>
2019-11-21 21:00:58 -0600

    start trampoline animations
    it does a squish when you land on it

--- Assets/Scripts/Trampoline.cs
+++ Assets/Scripts/Trampoline.cs
@@ -6,22 +6,35 @@ public class Trampoline : MonoBehaviour
 {
     public float bounceStrength;
 
+    Animator anim;
+
+    private void Start() {
+        anim = GetComponent<Animator>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player")) {
+            Bounced();
             collision.GetComponent<PlayerController>()?.BouncedOnTrampoline(bounceStrength);
         }
         else if (collision.CompareTag("Totem"))
         {
+            Bounced();
+
             GameObject other = collision.gameObject;
 
             Rigidbody2D rb2d = other.GetComponent<Rigidbody2D>();
             rb2d.velocity = new Vector2(rb2d.velocity.x, bounceStrength);
         }
         else if (collision.CompareTag("Hazard") && collision.GetComponent<Bullet>() != null) {
+            Bounced();
             collision.GetComponent<Bullet>().BouncedOnTrampoline(bounceStrength);
         }
     }
 
 
+    private void Bounced() {
+        anim.SetTrigger("Bounced");
+    }
 }
